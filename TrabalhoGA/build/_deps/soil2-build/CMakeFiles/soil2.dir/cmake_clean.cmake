65dcf704cdcae816bdedee07c295607623bc5342
Gabriela Bley <bley.gabriela@gmail.com>
2025-10-05 00:10:56 -0300

    background, pular e morrer feitos

---
+++ TrabalhoGA/build/_deps/soil2-build/CMakeFiles/soil2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/soil2.dir/src/SOIL2/SOIL2.c.obj"
+  "CMakeFiles/soil2.dir/src/SOIL2/SOIL2.c.obj.d"
+  "CMakeFiles/soil2.dir/src/SOIL2/image_DXT.c.obj"
+  "CMakeFiles/soil2.dir/src/SOIL2/image_DXT.c.obj.d"
+  "CMakeFiles/soil2.dir/src/SOIL2/image_helper.c.obj"
+  "CMakeFiles/soil2.dir/src/SOIL2/image_helper.c.obj.d"
+  "CMakeFiles/soil2.dir/src/SOIL2/wfETC.c.obj"
+  "CMakeFiles/soil2.dir/src/SOIL2/wfETC.c.obj.d"
+  "CMakeFiles/soil2.dir/src/common/common.cpp.obj"
+  "CMakeFiles/soil2.dir/src/common/common.cpp.obj.d"
+  "libsoil2.a"
+  "libsoil2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/soil2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
